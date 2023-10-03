9c4531b4ab0187725d6894e20214ff3837dbe388
Diego Quispe <diegoar.quispec@gmail.com>
2023-10-04 01:10:07 +0200

    Instalador agregado [wifi y python]

---
+++ scripts/install_python_env.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Check if the script is being run as root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root. Use 'sudo $0'." 1>&2
+    exit 1
+fi
+
+	# Move to the work folder
+cd ~
+cd Desktop/Edge_IoT/
+
+	# Update the package list
+apt-get update
+
+	# Upgrade installed packages
+apt-get upgrade -y
+
+	# Install nano editor
+apt-get install -y nano
+
+	# Install DMKS
+apt-get install -y dkms
+
+	# Install python 3.8
+apt-get install -y python3.8
+apt-get install -y python3.8-venv
+python3.8 -m venv focux_env
+
+	# Upgrade pip
+source focux_env/bin/activate
+cd focux_env/bin
+./pip install --upgrade pip
+
+	# Installing the packages
+	# for python scripts
+cd ~
+cd Desktop/Edge_IoT/scripts
+pip install -r packages.txt
