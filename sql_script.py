b90107520d4d5a61742827dba3b7547b8af04efe
linkperu78 <diegoar.quispec@gmail.com>
2023-12-04 13:13:46 -0500

    2.0 : Json values modified for pesaje adding

--- sql_script.py
+++ sql_script.py
@@ -2,11 +2,15 @@ import sql_library as sql
 import models as Model
 import datetime
 
+
 #init
 my_host_sql = sql.sql_host()
-name_db = "dato"
+name_db = "database"
 
-my_table_model = Model.salud_backup_model()
+salud_enviados      = Model.salud_backup_model
+salud_no_enviados   = Model.salud_model
+pesaje_enviados     = Model.pesaje_backup_model
+pesaje_no_enviados  = Model.pesaje_model
 
 new_data = {
     "P" : 0.0,
@@ -22,21 +26,18 @@ new_data_pesaje = {
     "Fecha"     : "2023-08-16 17:21:14",
 }
 
-# instance/dato.db
 my_host_sql.set_name_db(name_db)
-#my_host_sql.create_db()
+my_host_sql.create_db()
 
-
-#my_host_sql.delete_table(test_pesaje)
-
-
-#my_host_sql.create_table(my_table_model)
-
-#my_host_sql.insert_data(test_pesaje, new_data_pesaje)
-#my_host_sql.delete_table("Pesaje_TPI_2023_08_18")
+my_host_sql.create_table(salud_enviados)
+my_host_sql.create_table(salud_no_enviados)
+my_host_sql.create_table(pesaje_enviados)
+my_host_sql.create_table(pesaje_no_enviados)
 
 my_host_sql.check_db()
 
+#my_host_sql.insert_data(test_pesaje, new_data_pesaje)
+#my_host_sql.delete_table("Pesaje_TPI_2023_08_18")
 #my_host_sql.delete_all_tables()
 #my_host_sql.check_db()
 
