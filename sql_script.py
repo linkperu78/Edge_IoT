e2fe3c67bf92f0772141bfb7cf2c3c1fb78c17b1
linkperu78 <diegoar.quispec@gmail.com>
2023-12-04 13:05:27 -0500

    Version standard

--- sql_script.py
+++ sql_script.py
@@ -6,8 +6,8 @@ import datetime
 my_host_sql = sql.sql_host()
 name_db = "dato"
 
-test_salud  = Model.create_model_salud_tpi("salud")
-test_pesaje = Model.create_model_pesaje_tpi("pesaje")
+my_table_model = Model.salud_backup_model()
+
 new_data = {
     "P" : 0.0,
     "F" : "1687564928",
@@ -30,8 +30,7 @@ my_host_sql.set_name_db(name_db)
 #my_host_sql.delete_table(test_pesaje)
 
 
-my_host_sql.create_table(test_salud)
-my_host_sql.create_table(test_pesaje)
+#my_host_sql.create_table(my_table_model)
 
 #my_host_sql.insert_data(test_pesaje, new_data_pesaje)
 #my_host_sql.delete_table("Pesaje_TPI_2023_08_18")
